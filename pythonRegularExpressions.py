bcc850e52492499557e2068fd68c11d9e934c005
Kenneth Murray <kwmwins@gmail.com>
2024-09-07 23:40:13 -0700

    Create pythonRegularExpressions.py

---
+++ pythonRegularExpressions.py
@@ -0,0 +1,25 @@
+'''
+1. Python Regular Expressions Deep Dive
+Objective: The aim of this assignment is to deepen your practical skills in Python's regular expressions, 
+enhancing your ability to process and manipulate text data. 
+You will tackle a variety of real-world scenarios, applying regex to extract, validate, 
+and transform text effectively.
+
+Task 1: Email Extraction Enhancement
+
+Problem Statement: You have a script that extracts email addresses from a text but needs to 
+be refined to exclude certain domains (e.g., '[exclude.com](http://exclude.com/)'). 
+Modify the script to extract all email addresses except those from the specified domain.
+
+
+Adapt the regex pattern to exclude email addresses from '[exclude.com](http://exclude.com/)'.
+
+Ensure the script still extracts all other valid email addresses. 
+'''
+import re
+
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+# does not include the selected user name (?!exclude\.com)
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@(?!exclude\.com)[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", text)
+print(emails)
